45c509040027d4efaf097196b926dd4340f60388
Trần Vũ <124017908+TeddyBrwn@users.noreply.github.com>
2023-11-11 16:26:56 +0700

    Update LoginComponent.js

--- client-customer/src/components/LoginComponent.js
+++ client-customer/src/components/LoginComponent.js
@@ -9,8 +9,8 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      txtUsername: "teddie",
-      txtPassword: "123",
+      txtUsername: "",
+      txtPassword: "",
     };
   }
   render() {
